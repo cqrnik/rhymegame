a7dd17364e6e886f2d8347acfbc10d3a5bc56d1c
Nikhil <ndeepak@ucsd.edu>
2023-02-14 15:08:18 -0800

    Update botrunner.py

--- botrunner.py
+++ botrunner.py
@@ -96,68 +96,91 @@ async def full_game_iteration(NUMBER_OF_WORDS, message, players):
     scores=[]
     for i in players:
         scores.append(0)
+    #for rounds in range(NUMBER_OF_WORDS): #loops for each round
     
-    # finds n random words from a list
-    chosen_words = random.sample(english_lower_list,(NUMBER_OF_WORDS*len(players)))
-    list_of_output_dicts = create_rhyme_results_list_of_dict(chosen_words)
-    for rounds in range(NUMBER_OF_WORDS): #loops for each round
-        for level,chosen_word in enumerate(chosen_words): #level:
-            for player in players: #loops for each player
+        # finds n random words from a list
+    chosen_words = random.sample(english_lower_list,(len(players))*NUMBER_OF_WORDS)#creates new word list
+    list_of_output_dicts = create_rhyme_results_list_of_dict(chosen_words)#creates solutions
+    game_players=[]
+    for rounds in range(NUMBER_OF_WORDS):
+        for each_player in players: #loops for each player
+            game_players.append(each_player)
+
+    temp=0
+    for level,chosen_word in enumerate(chosen_words): 
+        
+        player=game_players[temp]
+        guess=None 
+        displayword = chosen_word.replace("_"," ")
+
+        # asks user to input a word that rhymes
+        await message.channel.send(f"{player.mention}, Find a rhyme for \"{displayword}\": ")
+        #await asyncio.sleep(10) #timer?
+        while guess is None:  
+            async for prev in message.channel.history(limit=1):
+                if (prev.author == player):
+                    guess=prev.content
             
-                guess=None 
-                # asks user to input a word that rhymes
-                displayword = chosen_word.replace("_"," ")
-                await message.channel.send(f"{player.mention}, Find a rhyme for \"{displayword}\": ")
-                while guess is None:  
-                    async for prev in message.channel.history(limit=1):
-                        if (prev.author == player):
-                            guess=prev.content
-                    
-                if guess=="!exit":
-                    await exitGame(message)
-                    return
-                #check for 2nd start
-                if guess=="!start":
-                    await message.channel.send ("Can't start another game")
-                    guess="start"
-                
+        #check for exit
+        if guess=="!exit":
+            await exitGame(message)
+            return
+
+        #check for 2nd start
+        if guess=="!start":
+            await message.channel.send ("Can't start another game")
+            guess="start"
+        
 
 
-                # find the words that rhy nry hom cns
-                # look for the user input inside of these and determine score
-                res = check_matches(guess,list_of_output_dicts[level])
-                player_score=scores[players.index(player)]
+        # find the words that rhy nry hom cns
+        # look for the user input inside of these and determine score
+        res = check_matches(guess,list_of_output_dicts[level])
+        player_score=scores[players.index(player)]
 
-                # determine score
-                first_matched_type = [k for k, v in res.items() if v]
-                if first_matched_type:
-                    score_for_level = SCORES[first_matched_type[0]]
-                    #update total score
-                    player_score += score_for_level * res[first_matched_type[0]]['numSyllables']
+        # determine score
+        first_matched_type = [k for k, v in res.items() if v]
+        if first_matched_type:
+            score_for_level = SCORES[first_matched_type[0]]
+            #update total score
+            player_score += score_for_level * res[first_matched_type[0]]['numSyllables']
 
-                    await message.channel.send(f"""\
-                    {player.mention}'s guess: {guess}
-                    Type: {CODE_TO_NAME_MAPPING[first_matched_type[0]]}
-                    Score: {score_for_level} * {res[first_matched_type[0]]['numSyllables']} Syllable
-                    Total Score: {player_score}""")
-                else:
-                    await message.channel.send(f"""\
-                    Your guess: {guess}
-                    Type: NO MATCH
-                    Score: 0
-                    Total Score: {player_score}""")
-                scores[players.index(player)]=player_score
+            await message.channel.send(f"""\
+            {player.display_name}'s guess: {guess}
+            Type: {CODE_TO_NAME_MAPPING[first_matched_type[0]]}
+            Score: {score_for_level} * {res[first_matched_type[0]]['numSyllables']} Syllable
+            Total Score: {player_score}""")
+        else:
+            await message.channel.send(f"""\
+            Your guess: {guess}
+            Type: NO MATCH
+            Score: 0
+            Total Score: {player_score}""")
+        scores[players.index(player)]=player_score
+        temp+=1
 
-    await message.channel.send(f""" 
+    #scoreboard
+    end_message=(f""" 
                     Game of {NUMBER_OF_WORDS} words ended
                     ========Scores:========
-                    User: 
-                                            """)        
+                    User:""")       
+    for player in players: #
+        end_message+=(f"\n {player.display_name}: {scores[players.index(player)]}")
+    await message.channel.send(end_message)
+
+    #winner message
+    if (len(players)>1):
+        high_score=0
+        for score in scores:
+            if score> high_score:
+                high_score=score
+        
+        await message.channel.send(f"{players[scores.index(high_score)].mention} wins!")
+
+    
     
 
-     
-
-#command start testing
+#start command
 @bot.command()
 async def start(ctx):
     
@@ -175,6 +198,7 @@ async def start(ctx):
     
     message = await ctx.fetch_message(message.id)
 
+    #getting users
     for reaction in message.reactions:
         if reaction.emoji == '✅':
             async for user in reaction.users():
@@ -194,64 +218,7 @@ async def start(ctx):
         await message.channel.send(f"Setting up game for {N} words...")
         await full_game_iteration(N, ctx, players)
    
-    # # start command
-    # if message.content.startswith("!start"):
 
-    #         #create list of tuples for players in the game
-    #         players= []
-    #         gameRunning=True
-
-    #         players.append({
-    #             'user':message.author,
-    #             'score':0
-    #         })
-    #         await message.channel.send(f"""\
-    #             Rhyme Game
-    #             !join to join the game
-    #             !continue to finalize players
-    #             !cancel to exit
-    #         """)
-    #         check=False
-    #         while check==False:
-    #             async for prev in message.channel.history(limit=1):
-    #                 if (prev.content == "!join"):
-    #                     if [d['user'] for d in players]==prev.author: #check duplicate
-    #                         await message.channel.send(f"Player has already joined")
-    #                         return
-    #                     else: 
-    #                         print ("111111111111111111111111111111111111111111")
-    #                         players.append({    #add player
-    #                         'user':prev.author,
-    #                         'score':0
-    #                     })
-    #                 elif (prev.content == "!continue"): #exit player add
-    #                     check=True
-
-    #                 elif (prev.content == "!cancel"): #exit player add
-    #                     await exitGame()
-    #                     exit
-    #         print (players)         
-
-    #         await message.channel.send(f"Enter game length")
-    #         
-    #         N=None
-    #         while N is None:  
-    #             async for prev in message.channel.history(limit=1):
-    #                 if (prev.author == message.author and int(prev.content)<=50):
-    #                     N=int(prev.content)
-                    
-    #         if N==0:
-    #                     await exitGame()
-    #                     exit
-    #         else:
-                
-    #             await message.channel.send(f"Setting up game for {N} words...")
-    #             await full_game_iteration(N)
-    #             players=[]
-            
-    
-
-    
     
     
     
